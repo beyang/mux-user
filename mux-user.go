05a12ccbfc8f095c123637cd8bc80d6ead117208
Beyang Liu <beyang@sourcegraph.com>
2018-01-24 22:17:09 -0800

    langserver user

--- mux-user.go
+++ mux-user.go
@@ -1,9 +1,11 @@
 package muxuser
 
 import (
+	"github.com/sourcegraph/go-langserver.git/pkg/lsp"
 	"github.com/gorilla/mux.git"
 )
 
 func init() {
+	var p lsp.InitializeParams
 	mux.NewRouter()
 }
